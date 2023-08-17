13bbf187526cc45a57216d7420c3c05e51ccf972
oguzhanural <oguzhnural@gmail.com>
2023-08-17 18:01:17 +0300

    route groups test

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <div className="flex justify-center items-center p-4">
+            <Button size="default" variant="destructive">Click me</Button>
+        </div>
+    )
+  } 
