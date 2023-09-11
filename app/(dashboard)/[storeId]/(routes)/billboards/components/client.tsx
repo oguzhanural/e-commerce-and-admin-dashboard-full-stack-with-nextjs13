f325f54b85a63f241f1b171dc67902ede40c05a2
oguzhanural <oguzhnural@gmail.com>
2023-09-11 23:43:09 +0300

    billboard client

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { useParams, useRouter } from "next/navigation";
+import { BillboardColumn, columns } from "./columns";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+import { Billboard } from "@prisma/client";
+
+import { DataTable } from "@/components/ui/data-table";
+
+interface BillboardClientProps {
+    data: BillboardColumn[];
+}
+
+
+export const BillboardClient:React.FC<BillboardClientProps> = ({ data }) => {
+
+    const router = useRouter();
+    const params = useParams();
+
+
+    return (
+        <>
+            <div className="flex items-center justify-between">
+                <Heading
+                    title={`Billboards (${data.length})`}
+                    description="Managed billboards for your store"
+                />
+                <Button onClick={ () => router.push(`/${params.storeId}/billboards/new`)}>
+                    <Plus className="mr-2 h-4 w-4" />
+                    Add New
+                </Button> 
+            </div>
+            <Separator />
+            <DataTable
+                columns={columns}
+                data={data}
+            />
+            
+        </>
+    )
+}
