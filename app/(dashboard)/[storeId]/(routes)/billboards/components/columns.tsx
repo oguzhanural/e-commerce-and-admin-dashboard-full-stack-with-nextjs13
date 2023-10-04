b6f6035905a38c442fd90f20cde446fb6118aaf6
oguzhanural <oguzhnural@gmail.com>
2023-10-04 11:54:12 +0300

    billboards components columns

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { CellAction } from "./cell-action"
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type BillboardColumn = {
+  id: string
+  label: string
+  createdAt: string // in prisma our createdAt is not string type but we gonna modify when passing.
+}
+
+export const columns: ColumnDef<BillboardColumn>[] = [
+  {
+    accessorKey: "label",
+    header: "Label",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original}/>
+  }
+]
